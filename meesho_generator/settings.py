a41b90bb06c29ad1f1d60970c34530e37a4c54b5
viraj ajani <99244056+virajajani@users.noreply.github.com>
2026-01-26 22:17:46 +0530

    Server setup

--- meesho_generator/settings.py
+++ meesho_generator/settings.py
@@ -92,7 +92,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static & Media
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
